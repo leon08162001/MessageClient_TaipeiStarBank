d5460b645ee22865a160f1ed31864c7678af9fe0
LeonLee <Leon08162001@gmail.com>
2019-03-08 13:38:13 +0800

    update

--- MessageClient/Services/MQService.cs
+++ MessageClient/Services/MQService.cs
@@ -222,8 +222,8 @@ namespace MessageClient.Services
 
                 //Toast.MakeText(this, "MQJefferiesExcuReport Server 處理完成", ToastLength.Long).Show();
 
-                //Toast.MakeText(this, "親愛的客戶，您好!目前系統收到一筆來自MoneySQ的推播通知，詳細訊息請瀏覽訊息公告", ToastLength.Long).Show();
-                SnackbarWrapper.make(Android.App.Application.Context, "親愛的客戶，您好!目前系統收到一筆來自MoneySQ的推播通知，詳細訊息請瀏覽訊息公告", 3000).Show();
+                Toast.MakeText(this, "親愛的客戶，您好!目前系統收到一筆來自MoneySQ的推播通知，詳細訊息請瀏覽訊息公告", ToastLength.Long).Show();
+                //SnackbarWrapper.make(Android.App.Application.Context, "親愛的客戶，您好!目前系統收到一筆來自MoneySQ的推播通知，詳細訊息請瀏覽訊息公告", 3000).Show();
 
                 //var v = (Vibrator)Android.App.Application.Context.GetSystemService(Android.App.Application.VibratorService);
                 //v.Vibrate(250);
